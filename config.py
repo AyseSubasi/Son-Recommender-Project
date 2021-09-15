0753fa35a29b549b93986f8573bf36c5c6260efd
AyseSubasi <86684300+AyseSubasi@users.noreply.github.com>
2021-09-15 17:21:07 +0200

    spotify

---
+++ config.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+client_id="588ab651fa66464d8372cfe6943b0f8b"
+
+
+# In[5]:
+
+
+client_secret= "40fde9f2b92a4313a580017b0871414c"
+
+
+# In[ ]:
+
+
+
+
